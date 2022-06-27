90d11814b87a280d6c009b776d8e14fd68a08361
depacifier <milkybekele@example.com>
2022-06-27 12:39:24 +0300

    updated config file and added some trial python files

---
+++ checkfiles.py
@@ -0,0 +1,5 @@
+import pickle
+
+load_data = pickle.load(open('./retry/f368-376.p', "rb"))
+
+print(load_data)
---
+++ python_code_trials.py
@@ -0,0 +1,9 @@
+# asbdjabsdkjanskdjaskjdnaslkdjnaslkjdnalksndlkasndlkasndlkasndlansdlkansdlknsadlknasldknalskdnlsakndlkasndlkasndlkansdlkasndlaksndlkasnd
+a = 4
+
+
+def foo(x, y, z):
+    print(x, y, z)
+
+    # This is a real comment.
+    return False
